1112caa81ff3c71d7dce1aa6ebaa125ff0a4c038
Sean Steel <seanrsteel@gmail.com>
2021-01-08 11:12:52 -0700

    Begin working on adding tones to poems, WIP:

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,8 @@ class SearchController < ApplicationController
   def index
     author  = params[:author]
     @poetry = SearchFacade.search_poetry(author, 10)
+
+
+    # require 'pry', binding.pry
   end
 end
